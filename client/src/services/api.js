3987e815daf70f93819c31e1c35e78fe2ccad3da
Raunak Prajapati <RAUNAKPRAJAPATI111@gmail.com>
2025-12-31 19:12:49 +0530

    updated backend cors

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,9 +1,8 @@
 import axios from 'axios';
 
-const BASE_URL =
-  import.meta.env.DEV
-    ? 'http://localhost:5000/api' // Local backend during development
-    : 'https://contentai-mmuz.onrender.com/api'; // Production backend
+const BASE_URL = import.meta.env.DEV
+  ? 'http://localhost:5000/api'
+  : import.meta.env.VITE_API_URL;
 
 const api = axios.create({
   baseURL: BASE_URL,
--- server/src/index.js
+++ server/src/index.js
@@ -17,11 +17,16 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Security middleware
-app.use(helmet());
+app.use(
+  helmet({
+    crossOriginResourcePolicy: { policy: "cross-origin" }
+  })
+);
+
 app.use(cors({
   origin: process.env.NODE_ENV === 'production' 
     ? process.env.FRONTEND_URL 
-    : ['http://localhost:5173', 'http://localhost:3000'],
+    : ['http://localhost:5173', 'http://localhost:5174','http://localhost:4173', 'https://content-ai-roan.vercel.app/'],
   credentials: true
 }));
 
